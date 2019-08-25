75dbe22934b344acca0bd2424255851971334300
Jonathan Fraimorice <jonathanfr@wix.com>
2019-08-25 10:23:24 +0300

    first svelte project

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  let name = "world";
+
+  const calculate = seed => {
+    for (let i = 0; i < 20; i++) {
+      world += i;
+    }
+
+    return world;
+  };
+</script>
+
+<h1>Hello {calculate}! How are you?</h1>
